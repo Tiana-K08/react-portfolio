93f70a33ae1315d64d207b958e99a6682027d042
Tiana-K08 <tetkononenko2020@gmail.com>
2025-07-18 16:11:36 +0200

    Loop in function

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import PortfolioItem from './portfolio-item';
 
 export default class PortfolioContainer extends Component {
+  portfolioItems() {
+    const data = ['One', 'Two', 'Three', 'Four', 'Five'];
+    return data.map(item => {
+      return <PortfolioItem />;
+    });
+  }
   render() {
     return (
       <div>
         <h2>Portfolio items go here ...</h2>
-        <PortfolioItem />
+        {this.portfolioItems()}
       </div>
     );
   }
