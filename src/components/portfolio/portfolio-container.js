1d5e4f3e1de952b1f914a37145163d794d9c4e97
Tiana-K08 <tetkononenko2020@gmail.com>
2025-07-27 12:12:08 +0200

    this.setState

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -13,11 +13,19 @@ export default class PortfolioContainer extends Component {
         { title: 'Item 4' }
       ]
     };
+
+    // this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
   }
 
   portfolioItems() {
     return this.state.data.map(item => {
-      return <PortfolioItem title={item.title} />;
+      return <PortfolioItem title={item.title} url={"google.com"} />;
+    });
+  }
+
+  hendlePageTitleUpdate() {
+    this.setState({
+      pageTitle: 'Something Else'
     });
   }
 
@@ -26,6 +34,10 @@ export default class PortfolioContainer extends Component {
       <div>
         <h2>{this.state.pageTitle}</h2>
         {this.portfolioItems()}
+        <hr/>
+        <button onClick={() => this.hendlePageTitleUpdate()}>
+          Change Title
+        </button>
       </div>
     );
   }
