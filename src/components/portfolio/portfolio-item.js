2161e26b9d956f6b4d5311810147fb741c145d27
Tiana-K08 <tetkononenko2020@gmail.com>
2025-08-08 10:03:06 +0200

    refactoring to use destructuring

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export default function(props) {
+  const { id, description, thumb_image_url, logo } = props.item;
   return (
     <div>
-      <h3>{props.title}</h3>
-      <h4>{props.url}</h4>
-      <Link to={`/portfolio/${props.slug}`}>Link to details</Link>
+      <div>{description}</div>
+      <Link to={`/portfolio/${id}`}>Link to details</Link>
     </div>
   );
 }
