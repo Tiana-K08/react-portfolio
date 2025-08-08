b3e23c34d4733c5297e4baf77e98e882f508b3cd
Tiana-K08 <tetkononenko2020@gmail.com>
2025-08-08 10:48:33 +0200

    added img

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -5,6 +5,8 @@ export default function(props) {
   const { id, description, thumb_image_url, logo } = props.item;
   return (
     <div>
+      <img src={thumb_image_url} alt="bck image" />
+      <img src={logo} alt="Logo" />
       <div>{description}</div>
       <Link to={`/portfolio/${id}`}>Link to details</Link>
     </div>
