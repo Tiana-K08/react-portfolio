b8960c2120fcbfc5f59d8a3d31364422d6b7f8f4
Tiana-K08 <tetkononenko2020@gmail.com>
2025-08-08 16:14:03 +0200

    styles for navigation container

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -10,11 +10,18 @@ export default class NavigationContainer extends Component {
         return (
             <div className="nav-wrapper">
                 <div className="left-side">
-                    <NavLink exact to='/'>Home</NavLink>
-                    <NavLink to='/about-me'>About</NavLink>
-                    <NavLink to='/contact'>Contact</NavLink>
-                    <NavLink to='/blog'>Blog</NavLink>
-                    {false ? <button>Add Blog</button> : null}
+                    <div className="nav-link-wrapper">
+                        <NavLink exact to='/'>Home</NavLink>
+                    </div>
+                    <div className="nav-link-wrapper">
+                        <NavLink to='/about-me'>About</NavLink>
+                    </div>
+                    <div className="nav-link-wrapper">
+                        <NavLink to='/contact'>Contact</NavLink>
+                    </div>
+                    <div className="nav-link-wrapper">
+                        <NavLink to='/blog'>Blog</NavLink>
+                    </div>
                 </div>
                 <div className="right-side">TETIANA KONONENKO</div>
             </div>
