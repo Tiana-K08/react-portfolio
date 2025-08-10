50e402e7417647c9d1211c70846a5292b654649f
Tiana-K08 <tetkononenko2020@gmail.com>
2025-08-10 18:14:04 +0200

    clear header

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import moment from 'moment';
 import {
   BrowserRouter as Router,
   Route,
@@ -19,8 +18,6 @@ export default class App extends Component {
       <div className='app'>
         <Router>
           <div>
-            <h1>Tetiana Kononenko Portfolio</h1>
-            <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
             <NavigationContainer />
             <Switch>
               <Route exact path='/' component={Home} />
