dcb5bdae356a486a3614366bb87fab2d30fc9409
Tiana-K08 <tetkononenko2020@gmail.com>
2025-08-16 19:17:04 +0200

    dynamic link

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavigationContainer = props => {
+    const dynamicLink = (route, linkText) => {
+        return (
+            <div className="nav-link-wrapper">
+                <NavLink to='/blog' activeClassName="nav-link-active">Blog</NavLink>
+            </div>
+        );
+    }
+
   return (
     <div className="nav-wrapper">
       <div className="left-side">
@@ -14,12 +22,11 @@ const NavigationContainer = props => {
         <div className="nav-link-wrapper">
           <NavLink to='/contact' activeClassName="nav-link-active">Contact</NavLink>
         </div>
-        <div className="nav-link-wrapper">
-          <NavLink to='/blog' activeClassName="nav-link-active">Blog</NavLink>
-        </div>
+        {props.loggedInStatus ==='LOGGED_IN' ? dynamicLink('/blog', 'Blog') : null}
       </div>
       <div className="right-side">TETIANA KONONENKO</div>
     </div>
+    
   );
 }
 
