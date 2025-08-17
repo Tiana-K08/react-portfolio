d662ed550048a000d441440e024602d640f250d9
Tiana-K08 <tetkononenko2020@gmail.com>
2025-08-17 20:52:04 +0200

    Portfolio Manager State with API Call Data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PortfolioManager extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            PortfolioItems: [],
+        };
+    }
+
+    getPortfolioItems() {
+        axios.get('https://tetkononenko.devcamp.space/portfolio/portfolio_items', {
+            withCredentials: true,
+        }).then(response => {
+            this.setState({
+                PortfolioItems: [...response.data.portfolio_items]
+            });
+        }).catch(error => {
+            console.log("error from getPortfolioItems", error);
+        });
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
     render() {
         return (
             <div className="portfolio-manager-wrapper">
