dc3e0fc1219f3f34bacf6945b9a35f156daf1995
Tiana-K08 <tetkononenko2020@gmail.com>
2025-09-01 18:29:06 +0200

    Calling a Single Blog Item from the API in React

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class BlogDetail extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentId: this.props.match.params.slug,
+            blogItem: {}
+        };
+    }
+
+    getBlogItem() {
+        axios.get(`https://tetkononenko.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`, { withCredentials: true })
+    .then(response => {
+        console.log('getBlogItem response', response);
+    })
+    .catch(error => {
+        console.log('getBlogItem error', error);
+    });
+    }
+
+    componentDidMount() {
+        this.getBlogItem();
+    }
+
     render() {
+        console.log('currentId', this.state.currentId);
         return (
         <div>
             <h1>Blog detail</h1>
