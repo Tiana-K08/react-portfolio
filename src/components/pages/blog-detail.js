3352b970772ccff91b527fe9cf025331004fdeb1
Tiana-K08 <tetkononenko2020@gmail.com>
2025-09-01 18:42:16 +0200

    Rendering the Blog Details

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -14,7 +14,9 @@ export default class BlogDetail extends Component {
     getBlogItem() {
         axios.get(`https://tetkononenko.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`, { withCredentials: true })
     .then(response => {
-        console.log('getBlogItem response', response);
+        this.setState({
+            blogItem: response.data.portfolio_blog
+        });
     })
     .catch(error => {
         console.log('getBlogItem error', error);
@@ -26,10 +28,19 @@ export default class BlogDetail extends Component {
     }
 
     render() {
+        const {
+            title,
+            content,
+            featured_image_url,
+            blog_status
+        } = this.state.blogItem;
+
         console.log('currentId', this.state.currentId);
         return (
         <div>
-            <h1>Blog detail</h1>
+            <h1>{title}</h1>
+            <img src={featured_image_url} />
+            <div>{content}</div>
         </div>
         );
     }
