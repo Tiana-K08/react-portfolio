4c46049d160697623fc0d869d05217398aa74290
Tiana-K08 <tetkononenko2020@gmail.com>
2025-09-02 08:42:50 +0200

    Add blog-post styles

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -37,10 +37,14 @@ export default class BlogDetail extends Component {
 
         console.log('currentId', this.state.currentId);
         return (
-        <div>
-            <h1>{title}</h1>
-            <img src={featured_image_url} />
-            <div>{content}</div>
+        <div className="blog-container">
+            <div className="content-container">
+                <h1>{title}</h1>
+                <div className="featured-image-wrapper">
+                    <img src={featured_image_url} />
+                </div>
+                <div className="content">{content}</div>
+            </div>
         </div>
         );
     }
