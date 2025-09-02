3b446cb84eeed8b2d312d51a866e18645c82a342
Tiana-K08 <tetkononenko2020@gmail.com>
2025-09-02 12:06:14 +0200

    Count of Total Records on the Server

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -8,7 +8,9 @@ class Blog extends React.Component {
         super();
 
         this.state = {
-            blogItems: []
+            blogItems: [],
+            totalCount: 0,
+            currentPage: 0
         };
 
         // this.getBlogItems = this.getBlogItems.bind(this);
@@ -30,10 +32,13 @@ class Blog extends React.Component {
     }
 
     getBlogItems() {
+        this.setState({ currentPage: this.state.currentPage + 1 });
+
         axios.get('https://tetkononenko.devcamp.space/portfolio/portfolio_blogs', { withCredentials: true })
             .then(response => {
                 this.setState({
-                    blogItems: response.data.portfolio_blogs
+                    blogItems: response.data.portfolio_blogs,
+                    totalCount: response.data.meta.total_records
                 });
             })
             .catch(error => {
