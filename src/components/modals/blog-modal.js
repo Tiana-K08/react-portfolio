542635ed87f0f0a13e06e9508ca8d20bcad98e3c
Tiana-K08 <tetkononenko2020@gmail.com>
2025-09-04 10:36:55 +0200

    Fixing Screen Reading Warning

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 
+Modal.setAppElement('.app-wrapper');
+
 export default class BlogModal extends Component {
     constructor(props) {
         super(props);
