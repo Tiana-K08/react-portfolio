f1fd3e12c9bd822ca319de56a366aabb343e3d92
Tiana-K08 <tetkononenko2020@gmail.com>
2025-09-04 10:29:41 +0200

    styles for modal

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -5,11 +5,26 @@ import Modal from 'react-modal';
 export default class BlogModal extends Component {
     constructor(props) {
         super(props);
+
+        this.customStyles = {
+            content: {
+                top: "50%",
+                left: "50%",
+                right: "auto",
+                marginRight: "-50%",
+                transform: "translate(-50%, -50%)",
+                width: "800px"
+            },
+            overlay: {
+                backgroundColor: "rgba(1, 1, 1, 0.75)"
+            }
+        };
     }
 
     render() {
         return(
             <Modal 
+                style={this.customStyles}
                 onRequestClose={() => {
                     this.props.handleModalClose();
                 }} 
