d3e415623177781f40cd125453e4174e85a30789
Tiana-K08 <tetkononenko2020@gmail.com>
2025-09-08 14:11:53 +0200

    Converting html to text

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ReactHtmlParser from 'react-html-parser'
 
 export default class BlogDetail extends Component {
     constructor(props) {
@@ -43,7 +44,7 @@ export default class BlogDetail extends Component {
                 <div className="featured-image-wrapper">
                     <img src={featured_image_url} />
                 </div>
-                <div className="content">{content}</div>
+                <div className="content">{ReactHtmlParser(content)}</div>
             </div>
         </div>
         );
