d4bdb8319942f352a84a3b3504f90d6077b281ce
Tiana-K08 <tetkononenko2020@gmail.com>
2025-09-08 10:34:28 +0200

    Fixing Set State on an Unmounted Component

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -35,12 +35,14 @@ export default class BlogForm extends Component {
     handleSubmit(event) {
         axios.post('https://tetkononenko.devcamp.space/portfolio/portfolio_blogs', this.buildForm(), { withCredentials: true })
         .then(response => {
-            this.props.handleSuccessfullFormSubmission(response.data.portfolio_blog);
-
             this.setState({
                 title: '',
-                blog_status: ''
+                blog_status: '',
+                content: ''
             });
+
+            this.props.handleSuccessfullFormSubmission(response.data.portfolio_blog);
+
         })
         .catch(error => {
             console.log('handleSubmit for blog error', error)
