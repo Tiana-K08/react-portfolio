c7de614d1134a7d6e64476f3313b25ec59df417a
Tiana-K08 <tetkononenko2020@gmail.com>
2025-09-11 13:02:09 +0200

    Layout About-page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 export default function() {
     return (
-        <div>
-        About
+        <div className="content-page-wrapper">
+            <div className="left-column" 
+            style={{
+                backgroundColor: '#008080'
+            }}
+            >
+                {/* TODO: img */}
+            </div>
+            <div className="right-column">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quod consectetur laborum eum. Cumque vel minus, necessitatibus facilis unde ipsa rerum quo harum facere nam consequatur. Omnis vel consequatur sequi.
+                Voluptatum officiis, adipisci fuga quod odit veritatis? Libero quia explicabo, repellat earum minus doloribus ipsam fugiat quam rerum quidem veritatis! Voluptatem impedit repellendus sunt, odit in ex maxime rem non.
+                Incidunt omnis sequi fugit laudantium similique quaerat cumque? Laborum et quasi ipsam in quae veritatis. Nihil, pariatur distinctio suscipit vitae et iusto eum obcaecati, optio aliquid perspiciatis tempora laudantium laboriosam!
+                Voluptatibus eos, ipsum doloribus labore mollitia aliquam soluta ex ullam enim culpa ad aliquid, voluptatum beatae inventore saepe quidem fugit pariatur nesciunt dolorem impedit commodi voluptate consequatur! Illo, nihil illum.
+                Nulla voluptates illum eligendi expedita praesentium commodi quo amet possimus magni assumenda sapiente labore harum ullam corrupti nisi inventore, quibusdam optio corporis consequatur delectus. Distinctio id ducimus dolore quia sit.
+                Ipsum consectetur incidunt vel, minima, nisi neque fugiat sint ea earum quidem harum esse inventore explicabo beatae debitis deserunt. Quisquam similique fugit consequatur libero nulla adipisci, ratione omnis voluptatibus voluptatem. erspiciatis cum quam? Fuga, nobis!
+            </div>
         </div>
     );
 }
