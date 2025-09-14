e198a0064ac34f88dcd4e3ca98ae228aadc1404f
Tiana-K08 <tetkononenko2020@gmail.com>
2025-09-14 18:12:34 +0200

    Update homepage

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default function(props) {
-    return (
-        <div>
-        <h2>Portfolio Detail for {props.match.params.slug}</h2>
-        </div>
-    );
+import PortfolioContainer from "../portfolio/portfolio-container";
+import { error } from 'ajv/dist/vocabularies/applicator/dependencies';
+
+export default class PortfolioDetail extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+        this.getPortfolioItem();
+    }
+
+    getPortfolioItem() {
+        axios.get(`https://tetkononenko.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`, { withCredentials: true })
+        .then(response => {
+            console.log('jbcnjabcao');
+        }).catch(error => {
+            conso
+            console.log('тщ');
+        }
+
+        )
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+            </div>
+        );
+    }
+    
 }
